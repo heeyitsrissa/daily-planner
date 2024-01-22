c1a5debf6ad874d2ba63cd02d5feaecd04b90aee
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2024-01-22 13:42:50 -0500

    added my own styling to css

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,20 @@ const leadEl = $('.lead');
 const saveBtnEl = $('.saveBtn');
 const descriptionEl = $('.description');
 const timeBlockEl = $('.time-block');
-
+// const textEl = $('#text');
 
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
-  saveBtnEl.on('click', saveTextInput);
-  function saveTextInput(){
-    let textInput = document.querySelectorAll('.description').value;
-    descriptionEl.textContent = textInput;
-    localStorage.setItem('textInput', JSON.stringify(textInput));
-    
-  }
+  $('#button').click(function(){ 
+    $('.time-block').each(function() {
+      const schedule = $(this).attr('id');
+      console.log(schedule);
+    })
+})
+
+
+  
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -27,7 +29,6 @@ $(function () {
   // TODO: Add code to apply the past, present, or future class to each time
   function timeTravel(){
     const presentHour = (new Date()).getHours();
-    const blockedTime = timeBlockEl.attr('id');
     $('.time-block').addClass(function() {
       return +this.id === presentHour ? 'present' : +this.id < presentHour ? 'past' : 'future'; 
     });
